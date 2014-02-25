8a4f1e45397e537030858325d5183378dcc9d278
Eric Allen <eric.lee.allen@gmail.com>
2014-02-24 20:13:05 -0700

    Start from the beginning

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Amp10::Application.config.secret_key_base = 'ab18212a5615cdb42f273346cf3a8499f87e371e06ee8f221909ba6421933ec6fa8203a06c1db951c0a2a82304453698dfcec1e543450d5b8bc05f0b3fd002ee'
